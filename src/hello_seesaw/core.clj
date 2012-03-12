11163b124cee51bc5410c25b5584ebf9c81b05ee
Marc Peabody <marc@edgecase.com>
2012-03-12 16:27:16 -0400

    immediate feedback slider freq setter & start/stop

--- src/hello_seesaw/core.clj
+++ src/hello_seesaw/core.clj
@@ -2,28 +2,48 @@
   (:require [seesaw.core :as see] [seesaw.bind :as sb])
   (:use overtone.live))
 
-  (def f (see/frame :title "Get to know Seesaw" :on-close :exit))
+(definst foo [freq 220] (saw freq))
 
-  (defn display [content]
-    (see/config! f :content content)
-    content)
+(def f (see/frame :title "Ooooo Waaaaah" :on-close :exit))
 
-  (def b (see/button :text "Click here to make it stop!!!"))
-  (def bup (see/button :text "Set frequency to the slider position"))
-  (def slide (see/slider :min 220 :max 440))
-  (def slide-val (atom 220))
-  (sb/bind slide slide-val)
+(defn display [content]
+  (see/config! f :content content)
+  content)
 
-  (definst foo [freq 220] (saw freq))
-  (def x (foo))
+(def slide-val (atom 440))
+(def x (foo :freq @slide-val))
 
-  (defn -main [& args]
-    (see/invoke-later
-      (display (see/border-panel
-                 :north slide
-                 :center b
-                 :south bup))
-      (-> f see/pack! see/show!)
-      (see/listen b :action (fn [e] (kill x) (see/alert e "Thanks!")))
-      (see/listen bup :action (fn [e] (ctl foo :freq @slide-val)))
-      ))
+(def stop-button (see/button :text "Stop"))
+(def start-button (see/button :text "Start" :enabled? false))
+
+(defn click-stop [e]
+  (see/config! stop-button  :enabled? false)
+  (see/config! start-button :enabled? true)
+  (kill x)
+)
+(see/listen stop-button :action click-stop)
+
+(defn click-start [e]
+  (see/config! start-button :enabled? false)
+  (see/config! stop-button  :enabled? true)
+  (def x (foo :freq @slide-val))
+)
+(see/listen start-button :action click-start)
+
+(def slide (see/slider :min 220 :max 440 :value @slide-val))
+(def slide-text (see/label :text (str @slide-val "Hz")))
+
+(sb/bind slide-val (sb/property slide-text :text))
+(sb/bind slide-val (sb/transform #(str % "Hz")) (sb/property slide-text :text))
+(sb/bind slide slide-val)
+(sb/subscribe slide-val (fn [e] (ctl foo :freq @slide-val)))
+
+(defn -main [& args]
+  (see/invoke-later
+    (display (see/border-panel
+               :west (see/horizontal-panel :items [slide slide-text])
+               :south (see/horizontal-panel :items [stop-button start-button])))
+    (-> f see/pack! see/show!)
+  )
+)
+
