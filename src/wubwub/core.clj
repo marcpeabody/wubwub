a37096a2b0057de95727e924defd7321a7ffe635
Marc Peabody <marc@edgecase.com>
2012-03-27 14:43:07 -0400

    Added dubsteb. Slider now controls wobble.

--- src/wubwub/core.clj
+++ src/wubwub/core.clj
@@ -1,17 +1,17 @@
-(ns hello-seesaw.core
+(ns wubwub.core
   (:require [seesaw.core :as see] [seesaw.bind :as sb])
-  (:use overtone.live))
+  (:use overtone.live wubwub.wubs))
 
 (definst foo [freq 220] (saw freq))
 
-(def f (see/frame :title "Ooooo Waaaaah" :on-close :exit))
+(def f (see/frame :title "Wub Wub" :on-close :exit))
 
 (defn display [content]
   (see/config! f :content content)
   content)
 
-(def slide-val (atom 440))
-(def x (foo :freq @slide-val))
+(def wobble-val (atom 1))
+(def wub (dubstep))
 
 (def stop-button (see/button :text "Stop"))
 (def start-button (see/button :text "Start" :enabled? false))
@@ -19,31 +19,30 @@
 (defn click-stop [e]
   (see/config! stop-button  :enabled? false)
   (see/config! start-button :enabled? true)
-  (kill x)
+  (stop)
 )
 (see/listen stop-button :action click-stop)
 
 (defn click-start [e]
   (see/config! start-button :enabled? false)
   (see/config! stop-button  :enabled? true)
-  (def x (foo :freq @slide-val))
+  (def wub (dubstep :wobble @wobble-val))
 )
 (see/listen start-button :action click-start)
 
-(def slide (see/slider :min 220 :max 440 :value @slide-val))
-(def slide-text (see/label :text (str @slide-val "Hz")))
+(def wobble-slide (see/slider :min 1 :max 10 :value @wobble-val))
+(def wobble-slide-text (see/label :text (str @wobble-val)))
 
-(sb/bind slide-val (sb/property slide-text :text))
-(sb/bind slide-val (sb/transform #(str % "Hz")) (sb/property slide-text :text))
-(sb/bind slide slide-val)
-(sb/subscribe slide-val (fn [e] (ctl foo :freq @slide-val)))
+(sb/bind wobble-val (sb/property wobble-slide-text :text))
+(sb/bind wobble-slide wobble-val)
+(sb/subscribe wobble-val (fn [e] (ctl wub :wobble @wobble-val)))
+
 
 (defn -main [& args]
   (see/invoke-later
     (display (see/border-panel
-               :west (see/horizontal-panel :items [slide slide-text])
+               :west (see/horizontal-panel :items [wobble-slide wobble-slide-text])
                :south (see/horizontal-panel :items [stop-button start-button])))
     (-> f see/pack! see/show!)
   )
 )
-
