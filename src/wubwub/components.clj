818f12bb9d7cc3094c70596bd9f1a9bd0e78397b
Marc Peabody <marc@edgecase.com>
2012-03-27 16:31:04 -0400

    componentized for reusability

---
+++ src/wubwub/components.clj
@@ -0,0 +1,27 @@
+(ns wubwub.components
+  (:require [seesaw.core :as see] [seesaw.bind :as sb]))
+
+(defn slider [model-val min max cb]
+  (def slide (see/slider :min min :max max :value @model-val))
+  (def slide-text (see/label :text (str @model-val)))
+  (sb/bind model-val (sb/property slide-text :text))
+  (sb/bind slide model-val)
+  (sb/subscribe model-val (cb model-val))
+  (see/horizontal-panel :items [slide slide-text])
+)
+
+(defn toggle-buttons [text-one action-one text-two action-two]
+  (def one-button (see/button :text text-one))
+  (def two-button (see/button :text text-two :enabled? false))
+  (see/listen one-button :action (fn [e]
+    (see/config! one-button :enabled? false)
+    (see/config! two-button :enabled? true)
+    (action-one)
+  ))
+  (see/listen two-button :action (fn [e]
+    (see/config! one-button :enabled? true)
+    (see/config! two-button :enabled? false)
+    (action-two)
+  ))
+  (see/horizontal-panel :items [one-button two-button])
+)
--- src/wubwub/core.clj
+++ src/wubwub/core.clj
@@ -1,48 +1,28 @@
 (ns wubwub.core
   (:require [seesaw.core :as see] [seesaw.bind :as sb])
-  (:use overtone.live wubwub.wubs))
-
-(definst foo [freq 220] (saw freq))
+  (:use overtone.live wubwub.wubs wubwub.components))
 
 (def f (see/frame :title "Wub Wub" :on-close :exit))
-
 (defn display [content]
   (see/config! f :content content)
   content)
 
-(def wobble-val (atom 1))
 (def wub (dubstep))
 
-(def stop-button (see/button :text "Stop"))
-(def start-button (see/button :text "Start" :enabled? false))
+(def wobble-val (atom 1))
 
-(defn click-stop [e]
-  (see/config! stop-button  :enabled? false)
-  (see/config! start-button :enabled? true)
-  (stop)
-)
-(see/listen stop-button :action click-stop)
+(def slider-panel (slider wobble-val 1 10
+  (fn [model-val] (fn [e] (ctl wub :wobble @model-val))
+)))
 
-(defn click-start [e]
-  (see/config! start-button :enabled? false)
-  (see/config! stop-button  :enabled? true)
-  (def wub (dubstep :wobble @wobble-val))
-)
-(see/listen start-button :action click-start)
-
-(def wobble-slide (see/slider :min 1 :max 10 :value @wobble-val))
-(def wobble-slide-text (see/label :text (str @wobble-val)))
-
-(sb/bind wobble-val (sb/property wobble-slide-text :text))
-(sb/bind wobble-slide wobble-val)
-(sb/subscribe wobble-val (fn [e] (ctl wub :wobble @wobble-val)))
-
+(def buttons-panel (toggle-buttons "Stop" stop
+                                   "Start" (fn [] (def wub (dubstep :wobble @wobble-val)))))
 
 (defn -main [& args]
   (see/invoke-later
     (display (see/border-panel
-               :west (see/horizontal-panel :items [wobble-slide wobble-slide-text])
-               :south (see/horizontal-panel :items [stop-button start-button])))
+               :west slider-panel
+               :south buttons-panel))
     (-> f see/pack! see/show!)
   )
 )
